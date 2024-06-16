1578bec4088c2002ab608842d34f4504bf96fb51
dmihovch <dmihovch@gmail.com>
2024-06-15 22:21:43 -0400

    added global css + started form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../global.css';
+
+</script>
+
+<div class="input-container">
+    <form>
+        <label for="sentence-input">Write Something!</label>
+        <br><br>
+        <input type="text" name="sentence-input" placeholder="Something!">
+        <br><br>
+        <input type="submit" value="Let's get Giffy">
+    </form>
+</div>
+
+<style>
+    .input-container {
+        position:absolute;
+        top:50%;
+        left:50%;
+        transform: translate(-50%,-50%);
+        padding: 10px;
+        border:2px solid white;
+        color:white;
+    }
+</style>
