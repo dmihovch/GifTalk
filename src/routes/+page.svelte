f1c8c10143bbf03ec0287d791ddd042b320da4d6
Dan Mihovch <dmihovch@gmail.com>
2024-06-16 12:56:33 -0400

    reformatted input component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,10 @@
 <script>
     import '../global.css';
-
+    import Input from './input.svelte';
 </script>
 
-<div class="input-container">
-    <form>
-        <label for="sentence-input">Write Something!</label>
-        <br><br>
-        <input type="text" name="sentence-input" placeholder="Something!">
-        <br><br>
-        <input type="submit" value="Let's get Giffy">
-    </form>
-</div>
+<Input />
 
 <style>
-    .input-container {
-        position:absolute;
-        top:50%;
-        left:50%;
-        transform: translate(-50%,-50%);
-        padding: 10px;
-        border:2px solid white;
-        color:white;
-    }
+   
 </style>
